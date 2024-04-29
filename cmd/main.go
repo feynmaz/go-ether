92d7be90e0a8a77a14778c33c9076ca0d10c6454
feynmaz <feynmaz@gmail.com>
2024-04-29 20:19:00 +0500

    get balance

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/feynmaz/go-ether/config"
 	log "github.com/sirupsen/logrus"
@@ -33,7 +34,16 @@ func run() error {
 		return fmt.Errorf("failed to get block: %w", err)
 	}
 
-	fmt.Println(block.Number())
+	fmt.Println("The latest block number:", block.Number())
+
+	addr := "0xB61Ff46584078A85246B4520173b3640Db871666"
+	address := common.HexToAddress(addr)
+
+	balance, err := client.BalanceAt(ctx, address, nil)
+	if err != nil {
+		return fmt.Errorf("failed to get balance: %w", err)
+	}
+	fmt.Printf("The %s address balance: %v \n", addr, balance)
 
 	return nil
 }
