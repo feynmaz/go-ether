7d927d15f4f80231cf00a21c3af2c274e6523f1a
feynmaz <feynmaz@gmail.com>
2024-04-29 20:38:01 +0500

    generate and encode private key

--- wallet/main.go
+++ wallet/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
 
 func main() {
-	fmt.Println("wallet")
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		return fmt.Errorf("failed to generate key: %w", err)
+	}
+
+	privateKeyData := crypto.FromECDSA(privateKey)
+	fmt.Println(hexutil.Encode(privateKeyData))
+
+	return nil
 }
